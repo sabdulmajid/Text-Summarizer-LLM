4a4569c70801fe43effd790886e5021689f9de91
sabdulmajid <ayman.hasib@outlook.com>
2023-05-14 21:07:05 +0300

    calculating word frequencies

--- LLMcode.py
+++ LLMcode.py
@@ -8,7 +8,7 @@ def summarize_text(file_path, n):
     # read text from file
     with open(file_path, 'r') as f:
         text = f.read()
-        
+
     # tokenize the text into words and sentences
     words = word_tokenize(text.lower())
     sentences = sent_tokenize(text)
@@ -17,3 +17,12 @@ def summarize_text(file_path, n):
     stop_words = set(stopwords.words('english'))
     words = [word for word in words if word not in stop_words and word.isalnum()]
     sentences = [sentence for sentence in sentences if sentence.isalnum()]
+
+    # calculate word frequencies and scores
+    word_freq = defaultdict(int)
+    word_scores = defaultdict(int)
+    for word in words:
+        word_freq[word] += 1
+    max_freq = max(word_freq.values())
+    for word in word_freq.keys():
+        word_scores[word] = word_freq[word] / max_freq
